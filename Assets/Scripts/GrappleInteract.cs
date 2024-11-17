7a36f2ffa839b48b230be129d7113ad0b95ae2b9
timurkoro <timurkorovi@gmail.com>
2024-11-17 14:32:55 -0500

    Added E prompt to grapple

---
+++ Assets/Scripts/GrappleInteract.cs
@@ -0,0 +1,37 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using static Unity.Collections.Unicode;
+
+public class GrappleInteract : MonoBehaviour
+{
+    public GameObject Canvas;
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Canvas.SetActive(false);
+    }
+    private void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Playerareatrigger player = collision.GetComponent<Playerareatrigger>();
+        if (player != null) // it is a player
+        {
+           
+            Canvas.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Playerareatrigger player = collision.GetComponent<Playerareatrigger>();
+        if (player != null) // it is a player
+        {
+           
+            Canvas.SetActive(false);
+            
+        }
+    }
+}
