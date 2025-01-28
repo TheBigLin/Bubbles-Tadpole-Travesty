f2613a9134d89aeebd6df0c45f7239bbce106296
FlapJax06 <200616373@student.georgianc.on.ca>
2025-01-28 17:25:19 -0500

    ya
    pushing script attempt 1

--- Assets/FALLTHROUGHTILES.cs
+++ Assets/FALLTHROUGHTILES.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class FALLTHROUGHTILES : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public float playertimer = 3.0f; // how long the player has to stand (in seconds)
+    public float timeforfall = 0f; //timer for player
+
+    private void TIMERSETUP(Collider other)
     {
-        
+        // Check if the player is the one standing on the tile
+        if (other.CompareTag("Player"))
+        {
+            playerOnTile = true;
+            timer += Time.deltaTime; // Increase the timer
+
+          
+            if (timeforfall >= FallThroughNow) ;
+            {
+                FallThroughNow();
+            }
+        }
+    }
+    private void FallThroughNow()
+    {
+
+        GetComponent<Collider>().enabled = false;
+
+
     }
 }
