751abb4c3a4f3c149cdd4746ae4b2f50ce1fe641
FlapJax06 <200616373@student.georgianc.on.ca>
2025-01-28 17:28:46 -0500

    ATTEMPT 2 TILE
    PUSH 2

--- Assets/FALLTHROUGHTILES.cs
+++ Assets/FALLTHROUGHTILES.cs
@@ -5,14 +5,14 @@ public class FALLTHROUGHTILES : MonoBehaviour
 
     public float playertimer = 3.0f; // how long the player has to stand (in seconds)
     public float timeforfall = 0f; //timer for player
-
+    private bool playerOnTile = false;
     private void TIMERSETUP(Collider other)
     {
         // Check if the player is the one standing on the tile
         if (other.CompareTag("Player"))
         {
             playerOnTile = true;
-            timer += Time.deltaTime; // Increase the timer
+            timeforfall += Time.deltaTime; // Increase the timer
 
           
             if (timeforfall >= FallThroughNow) ;
