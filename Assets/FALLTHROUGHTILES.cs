3ff3d52d8bf7cf0ff45c73c1833824a0a4ad2b91
CK00321 <200600276@student.georgianc.on.ca>
2025-01-28 17:46:42 -0500

    Changes-In-The-Code

--- Assets/FALLTHROUGHTILES.cs
+++ Assets/FALLTHROUGHTILES.cs
@@ -3,25 +3,26 @@ using UnityEngine;
 public class FALLTHROUGHTILES : MonoBehaviour
 {
 
-    public float playertimer = 3.0f; // how long the player has to stand (in seconds)
+    public float playertimer = 30.0f; // how long the player has to stand (in seconds)
     public float timeforfall = 0f; //timer for player
-    private bool playerOnTile = false;
-    private void TIMERSETUP(Collider other)
+    //public bool playerOnTile = false;
+    public void TIMERSETUP(Collider other)
     {
         // Check if the player is the one standing on the tile
         if (other.CompareTag("Player"))
         {
-            playerOnTile = true;
+            //playerOnTile = true;
             timeforfall += Time.deltaTime; // Increase the timer
 
 
-            if (timeforfall >= playertimer);
+            if (timeforfall >= playertimer)
             {
                 FallThroughNow();
+                Debug.Log("Collider-For-Tile-Disables");
             }
         }
     }
-    private void FallThroughNow()
+    public void FallThroughNow()
     {
 
         GetComponent<Collider>().enabled = false;
