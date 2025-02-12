134daf8ae1c3faba652c3569be33601b4a66db57
CK00321 <200600276@student.georgianc.on.ca>
2025-02-12 18:04:33 -0500

    Scene-Update
    #Scene-Transition
    #Yarn-Spinner
    #Tile-System

---
+++ Assets/3-Playable-Data/UI-UX-Assets/UI-UX-Code/LevelLoadingThree.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoadingThree : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(LevelThree());
+    }
+
+    IEnumerator LevelThree()
+    {
+        yield return new WaitForSeconds(7.0f);
+        SceneManager.LoadScene("3-Level");
+    }
+}
