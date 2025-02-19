c94943e70cde6ea98bd63dbd5d35fe9f86837451
timurkoro <timurkorovi@gmail.com>
2025-02-19 18:53:03 -0500

    Quick-Push

---
+++ Assets/Scripts/LoadCredits.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadCredits : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Destroy(other.gameObject);
+            Debug.Log("Level-Two");
+            //Start-lEVEL-tWO
+            SceneManager.LoadScene("UI-UX-D1-Credits");
+
+        }
+    }
+}
